c8e7b611fe2e6fd4e46f11a3c1ccf4d8938949b3
jaybamania <90679000+jaykumarbamania@users.noreply.github.com>
2026-01-19 23:41:24 +0530

    Added skills section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Navbar from "./components/Navbar";
 import Hero from "./sections/Hero";
+import Skills from "./sections/Skills";
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
+      <Skills />
     </>
   );
 }
