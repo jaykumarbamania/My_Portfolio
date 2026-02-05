19191a78fec2f34c7632fc3e6836bca43e2baaa2
jaybamania <90679000+jaykumarbamania@users.noreply.github.com>
2026-02-06 00:59:41 +0530

    Implemented React router to route to new Case Study Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
+import { Route, Routes } from "react-router-dom";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
+import OMSCaseStudy from "./pages/OMSCaseStudy";
 import Contact from "./sections/Contact";
 import EngineeringPhilosophy from "./sections/EngineeringPhilosophy";
 import Experience from "./sections/Experience";
@@ -8,20 +10,45 @@ import Projects from "./sections/Projects";
 import Skills from "./sections/Skills";
 import SystemDesign from "./sections/SystemDesign";
 
-function App() {
+// function App() {
+//   return (
+//     <>
+//       <Navbar />
+//       <Hero />
+//       <EngineeringPhilosophy />
+//       <Experience />
+//       <SystemDesign />
+//       <Projects />
+//       <Skills />
+//       <Contact />
+//       <Footer />
+//     </>
+//   );
+// }
+
+const Home = () => (
+  <>
+    <Hero />
+    <EngineeringPhilosophy />
+    <Experience />
+    <SystemDesign />
+    <Projects />
+    <Skills />
+    <Contact />
+  </>
+);
+
+const App = () => {
   return (
     <>
       <Navbar />
-      <Hero />
-      <EngineeringPhilosophy />
-      <Experience />
-      <SystemDesign />
-      <Projects />
-      <Skills />
-      <Contact />
-      <Footer />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/oms-case-study" element={<OMSCaseStudy />} />
+      </Routes>
     </>
   );
-}
+};
 
 export default App;
